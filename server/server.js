06c62b77d277c120490b42946325bef5989543fc
ErGovindthakur <ergovindthakur@gmail.com>
2025-05-27 00:29:26 +0530

    writing some notes inside the code

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
+// ApolloServer: Main class to define your GraphQL server
 import { ApolloServer } from "@apollo/server";
+//startStandaloneServer: Helper to start the server easily without needing Express.
 import { startStandaloneServer } from "@apollo/server/standalone";
 
 // creating an array of users 
@@ -8,6 +10,13 @@ const users = [
      {id:3,name:'jay', age:32, isMarried:true},
      {id:4,name:'jea', age:42, isMarried:true},
 ]
+
+
+
+// 1. Query: Used to read/fetch data (e.g., get all users)
+// 2. Mutation: Used to write/change data (e.g., create user)
+// 3. type user: Defines the shape of a user object
+// 4. ID!, String!: The ! means "required"
 
 const typeDefs = `
 
@@ -30,11 +39,22 @@ isMarried:Boolean
 
 `;
 
+// 5. Resolvers
+// Resolvers define how to respond to the queries and mutations in typeDefs.
+
+// 6. 🔹 What to Learn:
+// parent, args: args contains the values passed in the query/mutation.
+
+// users.find(...): Finds a user by ID.
+
+// users.push(...): Adds a new user to the array.
+
 const resolvers = {
      Query: {
           getUser: () => {
                return users
           },
+
           getUserById: (parent,args) => {
                const id = args.id;
                return users.find((user)=>user.id === id)
@@ -43,6 +63,7 @@ const resolvers = {
 
      Mutation:{
          createUser:(parent,args)=>{
+
           const {name,age,isMarried} = args;
 
           const newUser = {
@@ -56,6 +77,7 @@ const resolvers = {
      }
 };
 
+// 7. Start server at port => 4000
 const server = new ApolloServer({ typeDefs, resolvers });
 
 const { url } = await startStandaloneServer(server, {
